57d53dac5abc151b1df6ed5ea8a7c68812fdbc22
KMartin207 <martinkothencz@gmail.com>
2026-04-24 11:21:11 +0200

    Delete km directory

--- km/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const hero = document.querySelector(".hero");
-    hero.style.opacity = 0;
-    setTimeout(() => {
-      hero.style.transition = "opacity 2s";
-      hero.style.opacity = 1;
-    }, 500);
-  });
